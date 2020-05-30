b33468566d7b4cf32893e4f215621cb40fb6fd25
Socrates5068 <65322699+Socrates5068@users.noreply.github.com>
2020-05-30 13:18:47 -0400

    first1

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var servicesRouter = require('./routes/services');
-var userRouter = require('./routes/user');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+var service = require('./routes/service');
 
 var app = express();
 
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/1.0', servicesRouter);
-app.use('/api/1.0', userRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use('/service', service);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +39,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-var port = 8000;
+const port = 8000;
 app.listen(port, () => {
   console.log("Corriendo " + port);
 });
